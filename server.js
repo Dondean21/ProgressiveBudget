f771b0f014d5d1811266a4503caefd10169902ff
Don Dean <Dondean3030@gmail.com>
2020-02-20 13:21:20 -0700

    fixed PORT

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 app.use(logger("dev"));
 
@@ -17,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI);
 
-mongoose.connect("mongodb://localhost/budget", {
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
